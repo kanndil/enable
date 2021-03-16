09ed19cfb8ca928e0f0838c05d60a2459a639b26
Youssef Kandil <63082375+youssefkandil@users.noreply.github.com>
2021-03-16 21:21:39 +0200

    Add files via upload

---
+++ my_app/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {}
+
+class Myapp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hello!'),
+    );
+  }
+}
