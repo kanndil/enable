4c6022fdadcf0a33599dd5a6f2d237de99ec9a32
Youssef Kandil <63082375+youssefkandil@users.noreply.github.com>
2021-03-17 17:07:33 +0200

    Files_to_add_classes

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,11 +1,56 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() => runApp(Myapp());
 
 class Myapp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
+    var interface_choices = ["Visual_button", "Voice Over_button"];
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+          backgroundColor: const Color(0xFF0F323A),
+          appBar: AppBar(
+            backgroundColor: const Color(0x30033049),
+            title: Container(
+              width: double.infinity,
+              child: Text(
+                "enable",
+                style: TextStyle(fontSize: 32, fontFamily: 'RobotoMono'),
+              ),
+            ),
+          ),
+          body: Column(
+            children: <Widget>[
+              ElevatedButton(
+                child: Text(
+                  interface_choices[0],
+                  style:
+                      TextStyle(fontSize: 24, color: const Color(0xFFFFFFFF)),
+                ),
+                onPressed: null,
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
+                ),
+              ),
+              ElevatedButton(
+                child: Text(
+                  interface_choices[1],
+                  style:
+                      TextStyle(fontSize: 24, color: const Color(0xFFFFFFFF)),
+                  textAlign: TextAlign.center,
+                ),
+                onPressed: null,
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
+                ),
+              ),
+            ],
+          ),
+          drawer: Drawer(
+              //child: // Populate the Drawer in the next step.
+              )),
     );
   }
 }
